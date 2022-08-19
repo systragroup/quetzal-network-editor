9f11304c5a20fae1d4c6de42a9c38bb56b49b503
sboivinsystra <sboivin@systra.com>
2022-08-19 09:39:21 -0400

    cleaning repo

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@page/Login.vue'
 import Home from '@page/Home.vue'
-import Map from '@page/Map.vue'
-import PostAdd from '@page/PostAdd.vue'
-import Post from '@page/Post.vue'
-import Category from '@page/Category.vue'
+
 
 Vue.use(Router)
 
@@ -26,30 +23,6 @@ export default new Router({
       component: Home,
       icon: 'fas fa-home',
       title: $gettext('Home'),
-    },
-    {
-      path: '/map',
-      name: Map.name,
-      component: Map,
-      icon: 'fas fa-map-marker',
-      title: $gettext('Map'),
-    },
-    {
-      path: '/add-post',
-      name: PostAdd.name,
-      component: PostAdd,
-      icon: 'fas fa-plus',
-      title: $gettext('Add a blog post'),
-    },
-    {
-      path: '/post/:id',
-      name: Post.name,
-      component: Post,
-    },
-    {
-      path: '/category/:id',
-      name: Category.name,
-      component: Category,
-    },
+    }
   ],
 })
