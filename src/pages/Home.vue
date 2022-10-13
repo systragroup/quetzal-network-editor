e8ebcb4ce654fe48c692d6acf8e65dc769ca60f3
sboivinsystra <sboivin@systra.com>
2022-10-13 17:27:58 -0400

    fix bug in new filter menu. lint everything

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,225 +9,243 @@ export default {
   components: {
     Map,
     SidePanel,
-},
+  },
   data () {
     return {
       nodes: {},
       links: {},
-      selectedTrips : [],
-      editorTrip : null,
-      actionsList :[{value:'Edit Line Info',
-                    name: $gettext('Edit Line Info')},
-                    {value: 'Cut Line From Node',
-                    name: $gettext('Cut Line From Node')},
-                    {value:'Cut Line At Node',
-                    name: $gettext('Cut Line At Node')},
-                    {value:'Extend Line Upward',
-                    name: $gettext('Extend Line Upward')},
-                    {value:'Extend Line Downward', 
-                    name: $gettext('Extend Line Downward')},
-                    {value:'Add Stop Inline', 
-                    name: $gettext('Add Stop Inline')}, 
-                    {value:'Move Stop', 
-                    name: $gettext('Move Stop')},
-                    {value:'Delete Stop', 
-                    name: $gettext('Delete Stop')},
-                    {value:'Edit Link Info', 
-                    name: $gettext('Edit Link Info')},
-                    {value:'Edit Node Info', 
-                    name: $gettext('Edit Node Info')}],            
-      action : null,
-      selectedNode : null,
-      selectedLink : null,
-      showDialog : false,
-      editorForm : {},
-      cursorPosition : [],
-      tripToDelete : null,
-      lingering : true,
+      selectedTrips: [],
+      editorTrip: null,
+      actionsList: [{
+        value: 'Edit Line Info',
+        name: $gettext('Edit Line Info'),
+      },
+      {
+        value: 'Cut Line From Node',
+        name: $gettext('Cut Line From Node'),
+      },
+      {
+        value: 'Cut Line At Node',
+        name: $gettext('Cut Line At Node'),
+      },
+      {
+        value: 'Extend Line Upward',
+        name: $gettext('Extend Line Upward'),
+      },
+      {
+        value: 'Extend Line Downward',
+        name: $gettext('Extend Line Downward'),
+      },
+      {
+        value: 'Add Stop Inline',
+        name: $gettext('Add Stop Inline'),
+      },
+      {
+        value: 'Move Stop',
+        name: $gettext('Move Stop'),
+      },
+      {
+        value: 'Delete Stop',
+        name: $gettext('Delete Stop'),
+      },
+      {
+        value: 'Edit Link Info',
+        name: $gettext('Edit Link Info'),
+      },
+      {
+        value: 'Edit Node Info',
+        name: $gettext('Edit Node Info'),
+      }],
+      action: null,
+      selectedNode: null,
+      selectedLink: null,
+      showDialog: false,
+      editorForm: {},
+      cursorPosition: [],
+      tripToDelete: null,
+      lingering: true,
     }
   },
   watch: {
-    
+
   },
   created () {
     this.links = this.$store.getters.links
     this.nodes = this.$store.getters.nodes
     this.editorTrip = this.$store.getters.editorTrip
-    
-    this.$store.commit('changeNotification',{text:$gettext("double click to edit line, right click to edit line properties"), autoClose:false})
 
+    this.$store.commit('changeNotification',
+      { text: $gettext('double click to edit line, right click to edit line properties'), autoClose: false })
   },
 
   methods: {
-    actionClick(event){
-
+    actionClick (event) {
       this.action = event.action
 
-      if ( this.action == 'Edit Line Info'){
+      if (this.action === 'Edit Line Info') {
         this.editorForm = structuredClone(this.$store.getters.editorLineInfo)
         this.lingering = event.lingering
         this.showDialog = true
-      }
-      else if ( this.action == 'Edit Link Info' ){
-         // link is clicked on the map
+      } else if (this.action === 'Edit Link Info') {
+        // link is clicked on the map
         this.selectedLink = event.selectedFeature.properties
-        // map selected link doesnt not return properties with null value. we need to get the links in the store with the selected index.
-        this.editorForm = this.$store.getters.editorLinks.features.filter((link) => link.properties.index == this.selectedLink.index)
+        // map selected link doesnt not return properties with null value. we need
+        // to get the links in the store with the selected index.
+        this.editorForm = this.$store.getters.editorLinks.features.filter(
+          (link) => link.properties.index === this.selectedLink.index)
         this.editorForm = this.editorForm[0].properties
 
         // filter properties to only the one that are editable.
         const filteredKeys = this.$store.getters.lineAttributes
         const uneditable = ['a', 'b', 'index', 'link_sequence']
-        let filtered = Object.keys(this.editorForm)
+        const filtered = Object.keys(this.editorForm)
           .filter(key => !filteredKeys.includes(key))
           .reduce((obj, key) => {
-            obj[key] = {'value': this.editorForm[key],
-                        'disabled': uneditable.includes(key)}
-            return obj;
-          }, {});
+            obj[key] = {
+              value: this.editorForm[key],
+              disabled: uneditable.includes(key),
+            }
+            return obj
+          }, {})
         this.editorForm = filtered
         this.showDialog = true
-      }
-      else if (this.action == 'Edit Node Info'){
+      } else if (this.action === 'Edit Node Info') {
         this.selectedNode = event.selectedFeature.properties
-        // map selected node doesnt not return properties with nanulln value. we need to get the node in the store with the selected index.
-        this.editorForm = this.$store.getters.editorNodes.features.filter((node) => node.properties.index == this.selectedNode.index)
+        // map selected node doesnt not return properties with nanulln value.
+        // we need to get the node in the store with the selected index.
+        this.editorForm = this.$store.getters.editorNodes.features.filter(
+          (node) => node.properties.index === this.selectedNode.index)
         this.editorForm = this.editorForm[0].properties
 
         // filter properties to only the one that are editable.
-        const uneditable = ['index'];
-        let filtered = Object.keys(this.editorForm)
+        const uneditable = ['index']
+        const filtered = Object.keys(this.editorForm)
           .reduce((obj, key) => {
-            obj[key] = {'value': this.editorForm[key],
-                        'disabled': uneditable.includes(key)}
-            return obj;
-          }, {});
+            obj[key] = {
+              value: this.editorForm[key],
+              disabled: uneditable.includes(key),
+            }
+            return obj
+          }, {})
         this.editorForm = filtered
         this.showDialog = true
-
-      }
-      else if (['Cut Line From Node','Cut Line At Node','Move Stop','Delete Stop'].includes(this.action)){
+      } else if (['Cut Line From Node', 'Cut Line At Node', 'Move Stop', 'Delete Stop'].includes(this.action)) {
         this.selectedNode = event.selectedFeature.properties
         this.applyAction()
-      }
-      else if (this.action == 'Add Stop Inline'){
+      } else if (this.action === 'Add Stop Inline') {
         this.selectedLink = event.selectedFeature.properties
         this.cursorPosition = event.lngLat
         this.applyAction()
       }
     },
 
-    applyAction(){
+    applyAction () {
       // click yes on dialog
       this.showDialog = false
-      switch(this.action){
-      case 'Cut Line From Node':
-        this.$store.commit('cutLineFromNode', {selectedNode: this.selectedNode}) 
-        break 
-      case 'Cut Line At Node':
-        this.$store.commit('cutLineAtNode', {selectedNode: this.selectedNode})  
-        break
-      case 'Delete Stop':
-        this.$store.commit('deleteNode', {selectedNode: this.selectedNode})
-        break
-      case 'Edit Link Info':
-        this.$store.commit('editLinkInfo', {selectedLinkId: this.selectedLink.index, info: this.editorForm})  
-        break
-      case 'Edit Node Info':
-        this.$store.commit('editNodeInfo', {selectedNodeId: this.selectedNode.index, info: this.editorForm})  
-        break
-      case 'Edit Line Info':
-        this.$store.commit('editLineInfo', this.editorForm)  
-        break
-      case 'deleteTrip':
-        this.$store.commit('deleteTrip', this.tripToDelete)
-        break
-      case 'Add Stop Inline':
-        this.$store.commit('addNodeInline', {selectedLink: this.selectedLink, lngLat: this.cursorPosition})
-        break
+      switch (this.action) {
+        case 'Cut Line From Node':
+          this.$store.commit('cutLineFromNode', { selectedNode: this.selectedNode })
+          break
+        case 'Cut Line At Node':
+          this.$store.commit('cutLineAtNode', { selectedNode: this.selectedNode })
+          break
+        case 'Delete Stop':
+          this.$store.commit('deleteNode', { selectedNode: this.selectedNode })
+          break
+        case 'Edit Link Info':
+          this.$store.commit('editLinkInfo', { selectedLinkId: this.selectedLink.index, info: this.editorForm })
+          break
+        case 'Edit Node Info':
+          this.$store.commit('editNodeInfo', { selectedNodeId: this.selectedNode.index, info: this.editorForm })
+          break
+        case 'Edit Line Info':
+          this.$store.commit('editLineInfo', this.editorForm)
+          break
+        case 'deleteTrip':
+          this.$store.commit('deleteTrip', this.tripToDelete)
+          break
+        case 'Add Stop Inline':
+          this.$store.commit('addNodeInline', { selectedLink: this.selectedLink, lngLat: this.cursorPosition })
+          break
       }
-      if (!this.lingering){
+      if (!this.lingering) {
         this.confirmChanges()
-        this.lingering=true
+        this.lingering = true
       }
-       
     },
-    cancelAction(){
+    cancelAction () {
       this.showDialog = false
-      if (!this.lingering){
+      if (!this.lingering) {
         this.abortChanges()
-        this.lingering=true
+        this.lingering = true
       }
     },
-    confirmChanges(){
+    confirmChanges () {
       // confirm changes on sidePanel, this overwrite Links in store.
       this.$store.commit('confirmChanges')
       // put editTrip and action to null.
-      this.editorTrip = null 
-      this.$store.commit('setEditorTrip',{tripId: null, changeBounds: false})
-      this.action=null
+      this.editorTrip = null
+      this.$store.commit('setEditorTrip', { tripId: null, changeBounds: false })
+      this.action = null
       // notification
-      this.$store.commit('changeNotification',{text:$gettext("modification applied"), autoClose:true,color:'success'})
-      
+      this.$store.commit('changeNotification',
+        { text: $gettext('modification applied'), autoClose: true, color: 'success' })
     },
-    abortChanges(){
+    abortChanges () {
       // unselect a trip for edition. nothing to commit on link here.
       // put editTrip and action to null.
-      this.editorTrip = null 
-      this.$store.commit('setEditorTrip',{tripId: null, changeBounds: false})
-      this.action=null
+      this.editorTrip = null
+      this.$store.commit('setEditorTrip', { tripId: null, changeBounds: false })
+      this.action = null
       // notification
-      this.$store.commit('changeNotification',{text:$gettext("modification aborted"), autoClose:true})
+      this.$store.commit('changeNotification', { text: $gettext('modification aborted'), autoClose: true })
     },
-    deleteButton(selectedTrip){
-      this.tripToDelete=selectedTrip
-      this.action='deleteTrip'
+    deleteButton (selectedTrip) {
+      this.tripToDelete = selectedTrip
+      this.action = 'deleteTrip'
       this.showDialog = true
     },
-    
+
   },
 }
 </script>
 <template>
-
   <section class="map-view">
     <v-dialog
-      persistent
       v-model="showDialog"
+      persistent
       max-width="290"
       @keydown.enter="applyAction"
-      @keydown.esc ="cancelAction"
+      @keydown.esc="cancelAction"
     >
       <v-card>
         <v-card-title class="text-h5">
-          {{ action == 'deleteTrip'? $gettext("Delete ") + ' '+ tripToDelete + '?': $gettext("Edit Properties")}}
+          {{ action == 'deleteTrip'? $gettext("Delete ") + ' '+ tripToDelete + '?': $gettext("Edit Properties") }}
         </v-card-title>
 
         <v-card-text v-if="['Edit Line Info', 'Edit Link Info', 'Edit Node Info'].includes(action)">
           <v-container>
-              <v-col cols="12" >
-                <v-text-field 
-                  v-for="(value, key) in editorForm" :key="key"
-                  v-model="value['value']"
-                  :label="key"
-                  :disabled="value['disabled']"
-                ></v-text-field>
-              </v-col>
+            <v-col cols="12">
+              <v-text-field
+                v-for="(value, key) in editorForm"
+                :key="key"
+                v-model="value['value']"
+                :label="key"
+                :disabled="value['disabled']"
+              />
+            </v-col>
           </v-container>
         </v-card-text>
 
-       
         <v-card-actions>
-          <v-spacer></v-spacer>
-          
+          <v-spacer />
 
           <v-btn
             color="grey"
             text
             @click="cancelAction"
-
           >
-            {{$gettext("Cancel")}}
+            {{ $gettext("Cancel") }}
           </v-btn>
 
           <v-btn
@@ -235,27 +253,24 @@ export default {
             text
             @click="applyAction"
           >
-            {{$gettext("Save")}}
+            {{ $gettext("Save") }}
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
+    <SidePanel
+      v-model="selectedTrips"
+      @confirmChanges="confirmChanges"
+      @abortChanges="abortChanges"
+      @deleteButton="deleteButton"
+      @propertiesButton="actionClick"
+    />
 
-  <SidePanel
-    v-model="selectedTrips" 
-    @confirmChanges="confirmChanges"
-    @abortChanges="abortChanges"
-    @deleteButton="deleteButton"
-    @propertiesButton="actionClick">
-  </SidePanel>
-
-  <Map 
-    :selectedTrips = "selectedTrips" 
-    @clickFeature = "actionClick">
-
-  </Map>
-  
+    <Map
+      :selected-trips="selectedTrips"
+      @clickFeature="actionClick"
+    />
   </section>
 </template>
 <style lang="scss" scoped>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,11 +1,9 @@
 <script>
 
-import links_example from '@static/links_exemple.geojson'
-import nodes_example from '@static/nodes_exemple.geojson'
-
-import links_base from '@static/links_base.geojson'
-import nodes_base from '@static/nodes_base.geojson'
-
+import linksExample from '@static/links_exemple.geojson'
+import nodesExample from '@static/nodes_exemple.geojson'
+import linksBase from '@static/links_base.geojson'
+import nodesBase from '@static/nodes_base.geojson'
 
 export default {
   name: 'Login',
@@ -15,248 +13,250 @@ export default {
       loadedLinks: {},
       loadedNodes: {},
       choice: null,
-      loading: {links:false,nodes:false},
+      loading: { links: false, nodes: false },
       showDialog: false,
     }
   },
-  mounted () {
-    //this.$store.commit('changeRoute', this.$options.name)
-  },
 
-  computed:{
-    filesAreLoaded(){return this.$store.getters.filesAreLoaded},
-    localFilesAreLoaded() {return (Object.keys(this.loadedLinks).length === 0 || Object.keys(this.loadedNodes).length === 0)? false: true},
+  computed: {
+    filesAreLoaded () { return this.$store.getters.filesAreLoaded },
+    localFilesAreLoaded () {
+      return !((Object.keys(this.loadedLinks).length === 0 || Object.keys(this.loadedNodes).length === 0))
+    },
   },
-  watch:{
-    loadedLinks(){
-      this.$store.commit('loadLinks',this.loadedLinks)
-      this.loading['links'] = false
+  watch: {
+    loadedLinks () {
+      this.$store.commit('loadLinks', this.loadedLinks)
+      this.loading.links = false
     },
-    loadedNodes(){
-
-      this.$store.commit('loadNodes',this.loadedNodes)
-      this.loading['nodes'] = false
+    loadedNodes () {
+      this.$store.commit('loadNodes', this.loadedNodes)
+      this.loading.nodes = false
     },
-    localFilesAreLoaded(val){
-      if (val){
+    localFilesAreLoaded (val) {
+      if (val) {
         this.login()
       }
-    }
+    },
 
+  },
+  mounted () {
+    // this.$store.commit('changeRoute', this.$options.name)
   },
   methods: {
     login () {
       // Leave time for animation to end (.animate-login and .animate-layer css rules)
       setTimeout(() => {
-        this.$router.push('/Home').catch(()=>{})
+        this.$router.push('/Home').catch(() => {})
       }, 1000)
     },
 
-    buttonHandle(choice){
+    buttonHandle (choice) {
       this.choice = choice
-      if (!this.filesAreLoaded)
-      {
-        if (choice == 'example'){
+      if (!this.filesAreLoaded) {
+        if (choice === 'example') {
           this.loadExample()
-        }
-        else if (choice=='newProject') {
+        } else if (choice === 'newProject') {
           this.newProject()
-        }
-        else {
+        } else {
           // read json links or node (depending on choice)
           this.$refs.fileInput.click()
         }
-      }
-      else{
-        this.showDialog=true
+      } else {
+        this.showDialog = true
       }
     },
-    applyDialog(){
-      if (this.choice == 'example'){
+    applyDialog () {
+      if (this.choice === 'example') {
         this.loadExample()
-      } 
-      else if (this.choice == 'newProject') {
-        this.newProject() 
-      } 
-      else {
+      } else if (this.choice === 'newProject') {
+        this.newProject()
+      } else {
         // this only happen when both files are loaded.
         // remove links and nodes from store. (and filesAreLoaded)
         this.$store.commit('unloadFiles')
         // handle click and open file explorer
         this.$refs.fileInput.click()
-
       }
       this.showDialog = !this.showDialog
-
     },
 
-    loadExample(){
-      this.loadedLinks = links_example
-      this.loadedNodes = nodes_example
+    loadExample () {
+      this.loadedLinks = linksExample
+      this.loadedNodes = nodesExample
     },
-    newProject(){
-      this.loadedLinks = links_base
-      this.loadedNodes = nodes_base
+    newProject () {
+      this.loadedLinks = linksBase
+      this.loadedNodes = nodesBase
     },
-
 
     onFilePicked (event) {
       this.loading[this.choice] = true
       const files = event.target.files
       // there is a file
-      if (!files.length){
-        this.loading[this.choice]=false
+      if (!files.length) {
+        this.loading[this.choice] = false
         return
       }
-      //it is a geojson
-      if (files[0].name.slice(-7) != 'geojson'){
-        this.loading[this.choice]=false
+      // it is a geojson
+      if (files[0].name.slice(-7) !== 'geojson') {
+        this.loading[this.choice] = false
         alert('file is not a geojson')
         return
       }
 
-      let fileReader = new FileReader()
+      const fileReader = new FileReader()
       fileReader.readAsText(files[0])
-      if (this.choice == 'links'){
-        fileReader.onload =  evt =>  {
-        try{
-            this.loadedLinks =  JSON.parse(evt.target.result)
-        } catch (e){ 
-          this.$store.commit('changeNotification',{text:e.message, autoClose:true,color:'red darken-2'}); 
-          this.loading[this.choice]=false }
+      if (this.choice === 'links') {
+        fileReader.onload = evt => {
+          try {
+            this.loadedLinks = JSON.parse(evt.target.result)
+          } catch (e) {
+            this.$store.commit('changeNotification', { text: e.message, autoClose: true, color: 'red darken-2' })
+            this.loading[this.choice] = false
+          }
+        }
+      } else if (this.choice === 'nodes') {
+        fileReader.onload = evt => {
+          try {
+            this.loadedNodes = JSON.parse(evt.target.result)
+          } catch (e) {
+            alert(e.message)
+            this.loading[this.choice] = false
+          }
+        }
       }
-      }else if (this.choice == 'nodes') {
-        fileReader.onload =  evt =>  {
-        try{
-            this.loadedNodes =  JSON.parse(evt.target.result)
-        } catch (e){  
-          alert(e.message)
-          this.loading[this.choice]=false }
-      }
-      }
-      
     },
   },
-
-  
 }
 </script>
 <template>
   <section>
-  <div class="layout">
-    <div
-      class="layout-overlay"
-      :class="{ 'animate-layer': loggedIn }"
-    />
-    <v-card
-      class="card"
-      :class="{ 'animate-login': loggedIn }"
-    >
-      <v-card-title class="title">
-        Quetzal Network Editor
-      </v-card-title>
-      <v-card-text :style="{textAlign: 'center'}">
-        <div class="subtitle">
-          {{ $gettext("Load Files") }}
-        </div>
-        <div>
-          {{ $gettext("Links and Nodes files must be geojson in EPSG:4326") }}
-        </div>
-        <v-btn
-          :loading="loading.links"
-          :color=" Object.keys(this.loadedLinks).length != 0? 'primary': 'normal'"
-          @click="buttonHandle('links')"
-        >
-          <v-icon
-            small
-            left
+    <div class="layout">
+      <div
+        class="layout-overlay"
+        :class="{ 'animate-layer': loggedIn }"
+      />
+      <v-card
+        class="card"
+        :class="{ 'animate-login': loggedIn }"
+      >
+        <v-card-title class="title">
+          Quetzal Network Editor
+        </v-card-title>
+        <v-card-text :style="{textAlign: 'center'}">
+          <div class="subtitle">
+            {{ $gettext("Load Files") }}
+          </div>
+          <div>
+            {{ $gettext("Links and Nodes files must be geojson in EPSG:4326") }}
+          </div>
+          <v-btn
+            :loading="loading.links"
+            :color=" Object.keys(loadedLinks).length != 0? 'primary': 'normal'"
+            @click="buttonHandle('links')"
           >
-          fa-solid fa-upload
-          </v-icon>
-          {{ $gettext('Links') }}
-        </v-btn>
+            <v-icon
+              small
+              left
+            >
+              fa-solid fa-upload
+            </v-icon>
+            {{ $gettext('Links') }}
+          </v-btn>
 
-        <v-btn
-          :loading="loading.nodes"
-          :color=" Object.keys(this.loadedNodes).length != 0? 'primary': 'normal'"
-          @click="buttonHandle('nodes')"
-        >
-          <v-icon
-            small
-            left
+          <v-btn
+            :loading="loading.nodes"
+            :color=" Object.keys(loadedNodes).length != 0? 'primary': 'normal'"
+            @click="buttonHandle('nodes')"
           >
-            fa-solid fa-upload
-          </v-icon>
-          {{ $gettext('Nodes') }}
-        </v-btn>
+            <v-icon
+              small
+              left
+            >
+              fa-solid fa-upload
+            </v-icon>
+            {{ $gettext('Nodes') }}
+          </v-btn>
 
-        <input
-          type="file"
-          style="display: none"
-          ref="fileInput"
-          accept=".geojson"
-          @change="onFilePicked"/>
-          
-      <div> 
-        <v-tooltip bottom open-delay="500">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn  small 
-              @click="buttonHandle('newProject')"
-              v-bind="attrs"
-              v-on="on">
-              {{ $gettext('New Project') }}
-            </v-btn> 
-          </template>
-          <span>{{ $gettext("Create a network from scratch")}}</span>
-        </v-tooltip>  
-      </div>
+          <input
+            ref="fileInput"
+            type="file"
+            style="display: none"
+            accept=".geojson"
+            @change="onFilePicked"
+          >
 
-      <div> 
-        <v-tooltip bottom open-delay="500">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn  x-small 
-              @click="buttonHandle('example')"
-              v-bind="attrs"
-              v-on="on">
-              {{ $gettext('Example') }}
-            </v-btn> 
-          </template>
-          <span>{{ $gettext("Load Montréal Example")}}</span>
-        </v-tooltip>  
-      </div>
-      </v-card-text>
-    </v-card>
-  </div>
-  <v-dialog
-      persistent
+          <div>
+            <v-tooltip
+              bottom
+              open-delay="500"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  small
+                  v-bind="attrs"
+                  @click="buttonHandle('newProject')"
+                  v-on="on"
+                >
+                  {{ $gettext('New Project') }}
+                </v-btn>
+              </template>
+              <span>{{ $gettext("Create a network from scratch") }}</span>
+            </v-tooltip>
+          </div>
+
+          <div>
+            <v-tooltip
+              bottom
+              open-delay="500"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  x-small
+                  v-bind="attrs"
+                  @click="buttonHandle('example')"
+                  v-on="on"
+                >
+                  {{ $gettext('Example') }}
+                </v-btn>
+              </template>
+              <span>{{ $gettext("Load Montréal Example") }}</span>
+            </v-tooltip>
+          </div>
+        </v-card-text>
+      </v-card>
+    </div>
+    <v-dialog
       v-model="showDialog"
+      persistent
       max-width="350"
       @keydown.enter="applyDialog"
       @keydown.esc="showDialog=false"
     >
       <v-card>
         <v-card-title class="text-h5">
-          {{ $gettext("Overwrite current Project ?")}}
+          {{ $gettext("Overwrite current Project ?") }}
         </v-card-title>
         <v-card-actions>
-          <v-spacer></v-spacer>
+          <v-spacer />
           <v-btn
             color="regular"
             @click="showDialog = !showDialog"
           >
-            {{$gettext("No")}}
+            {{ $gettext("No") }}
           </v-btn>
 
           <v-btn
             color="primary"
             @click="applyDialog"
           >
-            {{$gettext("Yes")}}
+            {{ $gettext("Yes") }}
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
-</section>
+  </section>
 </template>
 <style lang="scss" scoped>
 .layout {
