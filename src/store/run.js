6c83ea740e4d9ff63bd726778b2ec958465e3c0e
simon boivin <sboivin@systra.com>
2023-03-24 15:27:25 -0400

    load result after exceution

--- src/store/run.js
+++ src/store/run.js
@@ -1,5 +1,6 @@
 import { quetzalClient } from '@src/axiosClient.js'
 import s3 from '../AWSClient'
+import { classFile } from '../components/utils/utils.js'
 
 export default {
   namespaced: true,
@@ -49,6 +50,52 @@ export default {
       const params = await s3.readJson(payload.model, payload.path)
       state.parameters = params
     },
+    async getOutputs (context) {
+      const model = context.rootState.user.model
+      const scenario = context.rootState.user.scenario
+      const path = scenario + '/' + context.rootState.user.config.output_paths[0]
+      let filesNames = await s3.listFiles(model, path)
+      filesNames = filesNames.filter(name => name !== path)
+      const files = []
+      for (const file of filesNames) {
+        const content = await s3.readJson(model, file)
+        const name = file.split('/').slice(1).join('/')
+        files.push(classFile(name, content))
+      }
+      if (files.length > 0) {
+        context.commit('unloadLayers', null, { root: true })
+        files.filter(file => (['layerLinks', 'links', 'road_links'].includes(file.type))).forEach(
+          file => {
+            context.commit('loadLayer', {
+              fileName: file.fileName.slice(0, -8),
+              type: 'links',
+              links: file.data,
+            }, { root: true })
+          })
+        files.filter(file => (['layerNodes', 'nodes', 'road_nodes'].includes(file.type))).forEach(
+          file => {
+            context.commit('loadLayer', {
+              fileName: file.fileName.slice(0, -8),
+              type: 'nodes',
+              nodes: file.data,
+            }, { root: true })
+          })
+        // for zones. find the corresponding json file (mat) or nothing.
+        files.filter(file => (file.type === 'zones')).forEach(
+          file => {
+            const zoneData = file.data
+            const fileName = file.fileName.slice(0, -8)
+            let matData = files.filter(json => json.fileName.slice(0, -5) === fileName)[0]?.data
+            matData = matData || {}
+            context.commit('loadLayer', {
+              fileName: fileName,
+              type: 'zones',
+              zones: zoneData,
+              mat: matData,
+            }, { root: true })
+          })
+      }
+    },
     getSteps ({ state, commit }) {
       let data = { stateMachineArn: state.stateMachineArn }
       quetzalClient.client.post('/describe/model',
@@ -66,14 +113,14 @@ export default {
           commit('setSteps', steps)
         }).catch(
         err => {
-          console.log(err)
+          alert(err)
         })
     },
-    startExecution ({ state, commit, dispatch }) {
+    startExecution ({ state, commit, dispatch }, payload) {
       let data = {
         // eslint-disable-next-line no-useless-escape
         input: JSON.stringify({
-          scenario_path_S3: 'base/',
+          scenario_path_S3: payload.scenario + '/',
           launcher_arg: {
             scenario: 'base',
             training_folder: '/tmp',
@@ -92,7 +139,7 @@ export default {
           console.log(err)
         })
     },
-    pollExecution ({ commit, state }) {
+    pollExecution ({ commit, state, dispatch }) {
       const intervalId = setInterval(() => {
         let data = { executionArn: state.executionArn }
         quetzalClient.client.post('/describe',
@@ -101,8 +148,12 @@ export default {
           response => {
             this.status = response.data.status
             if (this.status === 'SUCCEEDED') {
-              commit('succeedExecution')
-              clearInterval(intervalId)
+              dispatch('getOutputs').then(
+                () => {
+                  commit('succeedExecution')
+                  clearInterval(intervalId)
+                },
+              ).catch(err => alert(err))
             } else if (['FAILED', 'TIMED_OUT', 'ABORTED'].includes(this.status)) {
               commit('terminateExecution')
               clearInterval(intervalId)
