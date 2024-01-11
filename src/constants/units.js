7bd8b9795f88144b1e862e917c0c21da5ee8f6b1
Simon Boivin <sboivin@systra.com>
2024-01-11 15:58:46 -0500

    WIP start working on speed,length time props

---
+++ src/constants/units.js
@@ -0,0 +1,12 @@
+// this file contains Hints on the different editable attributes
+
+const units = {
+
+  headway: 'secs',
+  time: 'secs',
+  length: 'm',
+  speed: '(km/h)',
+
+}
+
+export default units
