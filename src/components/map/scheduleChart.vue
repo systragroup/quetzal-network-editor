91977d724f761cefd0ebb85b10a155a52b4d08ff
simon fortin <sfortin@systra.com>
2024-03-29 09:08:50 -0400

    First Schedule Chart

--- src/components/map/scheduleChart.vue
+++ src/components/map/scheduleChart.vue
@@ -1,29 +1,92 @@
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Line as LineChart } from 'vue-chartjs'
+import 'chartjs-adapter-moment'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  TimeScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+)
 
+const scales = {
+  x: {
+    type: 'time',
+    min: '2021-11-06T23:39:30',
+  },
+}
+
+const data = {
+  datasets: [{
+    data: [{
+      x: '2021-11-06T23:39:30',
+      y: 'Montréal',
+    }, {
+      x: '2021-11-06T23:42:30',
+      y: 'Montréal',
+    }, {
+      x: '2021-11-07T00:10:28',
+      y: 'Ottawa',
+    }, {
+      x: '2021-11-07T00:15:28',
+      y: 'Ottawa',
+    }, {
+      x: '2021-11-07T00:59:28',
+      y: 'Peterborough',
+    }, {
+      x: '2021-11-07T01:04:28',
+      y: 'Peterborough',
+    }, {
+      x: '2021-11-07T01:33:28',
+      y: 'Toronto',
+    }, {
+      x: '2021-11-07T01:39:28',
+      y: 'Toronto',
+    },
+    ],
+  }],
+}
+
+const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: scales,
+  indexAxis: 'y',
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+}
 export default {
-  name: 'BarChart',
-  components: { Bar },
+  name: 'ScheduleChart',
+  components: { LineChart },
   data() {
     return {
-      chartData: {
-        labels: ['January', 'February', 'March'],
-        datasets: [{ data: [40, 20, 12] }],
-      },
-      chartOptions: {
-        responsive: true,
-      },
+      chartData: data,
+      chartOptions: options,
     }
   },
 }
 </script>
 <template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
+  <LineChart
     :data="chartData"
+    :options="chartOptions"
   />
 </template>
