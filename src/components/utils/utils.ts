73abcd3be0f560df4beb54102ac1aa9b09cf1bd6
Simon Boivin <sboivin@systra.com>
2025-02-10 16:36:16 -0500

    fix: Form type in ts

---
+++ src/components/utils/utils.ts
@@ -0,0 +1,17 @@
+// éonst $gettext = (s: string) => s
+
+import { GroupForm } from '@src/types/components'
+import { GeoJsonFeature } from '@src/types/geojson'
+
+export function getGroupForm (features: GeoJsonFeature[], lineAttributes: string[], uneditable: string[]) {
+  const form: GroupForm = {}
+  lineAttributes.forEach(key => {
+    const val = new Set(features.map(link => link.properties[key]))
+    form[key] = {
+      value: val.size > 1 ? '' : [...val][0],
+      disabled: uneditable.includes(key),
+      placeholder: val.size > 1,
+    }
+  })
+  return form
+}
