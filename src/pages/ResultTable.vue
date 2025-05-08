ba3b36480a9dd912d9ddf8928356e939b6445e64
Simon Boivin <sboivin@systra.com>
2025-05-08 14:11:12 -0400

    rewrite result tables to ts and change the laoding behaviour

--- src/pages/ResultTable.vue
+++ src/pages/ResultTable.vue
@@ -1,94 +1,99 @@
-<script>
+<script setup lang="ts">
 import s3 from '../AWSClient'
 import { csvJSON } from '@src/utils/io'
-import { ref, onMounted, toRaw } from 'vue'
+import { ref, onMounted, toRaw, computed } from 'vue'
 import { useIndexStore } from '@src/store/index'
 import { useUserStore } from '@src/store/user'
 import { useWebWorkerFn } from '@vueuse/core'
 import { orderBy } from 'lodash'
-const $gettext = s => s
+import { OtherFiles } from '@src/types/typesStore'
 const { workerFn: csvJSONWorker } = useWebWorkerFn(csvJSON)
 
-export default {
-  name: 'ResultTable',
-  setup () {
-    const store = useIndexStore()
-    const userStore = useUserStore()
-    const tables = ref([])
-    const message = ref('')
-    const skeletons = ref(0)
-    const numItems = ref(10)
-    const limit = 40 // 40mb max to display
+interface Header {
+  title: string
+  key: string
+  width: string
+}
 
-    async function getCSV () {
-      // get the list of CSV from output files.
-      // if its undefined (its on s3). fetch it.
-      const scenario = userStore.scenario + '/'
-      const otherFiles = store.otherFiles
-      const csvFiles = otherFiles.filter(file => file.extension === 'csv')
-      skeletons.value = csvFiles.length
+interface Table {
+  headers: Header[]
+  items: any
+  data: any
+  name: string
+  totalItems: number
 
-      for (const file of csvFiles) {
-        if (!(file.content instanceof Uint8Array)) {
-          file.content = await s3.readBytes(userStore.model, scenario + file.path, limit)
-        }
-      }
-      return csvFiles
+}
+const store = useIndexStore()
+const userStore = useUserStore()
+const tables = ref<Table[]>([])
+const skeletons = ref(0)
+const numItems = ref(10)
+const limit = 40 // 40mb max to display
+
+const csvFiles = computed(() => store.otherFiles.filter(file => file.extension === 'csv'))
+
+async function getFiles () {
+  // get the list of CSV from output files.
+  // if its undefined (its on s3). fetch it.
+  const scenario = userStore.scenario + '/'
+  skeletons.value = csvFiles.value.length
+
+  for (const file of csvFiles.value) {
+    if (!(file.content instanceof Uint8Array)) {
+      file.content = await s3.readBytes(userStore.model, scenario + file.path, limit)
     }
+  }
+}
 
-    onMounted(async () => {
-      store.changeLoading(true)
-      try {
-        const files = await getCSV()
+async function processCSV(file: OtherFiles) {
+  const name = file.path.slice(0, -4)
+  const data = (file.content !== null)
+    ? await csvJSONWorker(file.content.buffer)
+    : [{ too_large: `cannot display, more than ${limit} mb` }]
 
-        for (const file of files) {
-        // const name = file.path.split('/').splice(-1)[0].slice(0, -4)
-          const name = file.path.slice(0, -4)
-          const data = (file.content !== null)
-            ? await csvJSONWorker(file.content.buffer)
-            : [{ too_large: `cannot display, more than ${limit} mb` }]
+  const headers = Object.keys(data[0]).map(val => { return { title: val, key: val, width: '1%' } })
+  tables.value.push({ headers, items: data.slice(0, numItems.value), data, name, totalItems: data.length })
+  skeletons.value -= 1
+}
 
-          const headers = []
-          Object.keys(data[0]).forEach(val => headers.push({ title: val, key: val, width: '1%' }))
-          tables.value.push({ headers, items: data.slice(0, numItems.value), data, name, totalItems: data.length })
-          skeletons.value -= 1
-        }
-        store.changeLoading(false)
-        if (tables.value.length === 0) {
-          message.value = $gettext('Nothing to display')
-        }
-      } catch (err) {
-        console.error(err)
-        store.changeLoading(false)
-        skeletons.value = 0
-        message.value = $gettext('Nothing to display')
-      }
-    })
+onMounted(async () => {
+  store.changeLoading(true)
+  try {
+    await getFiles()
+    store.changeLoading(false)
+  } catch (err) {
+    console.error(err)
+    store.changeLoading(false)
+    skeletons.value = 0
+  }
+  for (const file of csvFiles.value) {
+    // const name = file.path.split('/').splice(-1)[0].slice(0, -4)
+    await processCSV(file)
+  }
+})
 
-    function getData (items, page, itemsPerPage, sortBy) {
-      const start = (page - 1) * itemsPerPage
-      const end = start + itemsPerPage
-      if (sortBy.length > 0) {
-        const sortKey = sortBy[0].key
-        const sortOrder = sortBy[0].order
-        items = orderBy(items, sortKey, sortOrder)
-      }
-      return items.slice(start, end)
-    }
-    async function loadItems ({ key, page, itemsPerPage, sortBy }) {
-      const items = toRaw(tables.value[key].data)
-      const paginated = getData(items, page, itemsPerPage, toRaw(sortBy))
-      tables.value[key].items = paginated
-    }
-    return { tables, message, numItems, loadItems, skeletons }
-  },
+function getData (items: any, page: number, itemsPerPage: number, sortBy: any) {
+  const start = (page - 1) * itemsPerPage
+  const end = start + itemsPerPage
+  if (sortBy.length > 0) {
+    const sortKey = sortBy[0].key
+    const sortOrder = sortBy[0].order
+    items = orderBy(items, sortKey, sortOrder)
+  }
+  return items.slice(start, end)
+}
+
+async function loadItems ({ key, page, itemsPerPage, sortBy }: any) {
+  const items = toRaw(tables.value[key].data)
+  const paginated = getData(items, page, itemsPerPage, toRaw(sortBy))
+  tables.value[key].items = paginated
 }
 
 </script>
 <template>
   <section class="layout">
     <p v-if="tables.length===0">
-      {{ $gettext(message) }}
+      {{ $gettext('nothing to display') }}
     </p>
     <div
       v-for="(table,key) in tables"
